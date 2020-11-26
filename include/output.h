aa506dc42bc3d1f67bfc76a99882fa7fccbc1cbe
xiuwen8686 <xiuwen8686@gmail.com>
2020-11-26 02:04:53 -0500

    folder function

--- include/output.h
+++ include/output.h
@@ -1,25 +1,39 @@
-
 #ifndef _OUTPUT_H
 #define _OUTPUT_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>     /*------xiu------*/
 #include "cal_method.h"
 #include "trace_analysis.h"
 
 
 
-
-char Ouput_FileName[50];
-char CSV[4];
+uint16_t normalization;
+char Output_FileName[50];
+char CSV[5];
 FILE *Output_File; 
+FILE *filelist[20];
+uint16_t file_cnt;
 
-void Output_Init();
 
-void output_entropy_csv(trace_info_t *info ,uint32_t ct );
+void create_folder();
+
+void NORfolder( char *Output_FolderName);
+
+void Output_Init(char *Output_FolderName);
+
+void output();
+
+void output_entropy_csv_origin();
+
+void output_entropy_csv_distinct();
+
+void output_entropy_csv_total();
 
 void Close_Output_File();
+
 
 
 
